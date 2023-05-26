87d4d6cae26274775518bbb6497431dff3b628b7
Mo-yang22 <97281303+Mo-yang22@users.noreply.github.com>
2023-05-26 11:46:59 +0800

    Update index_iterator.cpp

--- src/storage/index/index_iterator.cpp
+++ src/storage/index/index_iterator.cpp
@@ -38,7 +38,7 @@ auto INDEXITERATOR_TYPE::operator++() -> INDEXITERATOR_TYPE & {
   if (index_ >= cur_node_->GetSize() && cur_node_->GetNextPageId() != INVALID_PAGE_ID) {
     auto next_node =
         reinterpret_cast<LeafPage *>(buffer_pool_manager_->FetchPage(cur_node_->GetNextPageId())->GetData());
-    buffer_pool_manager_->UnpinPage(cur_node_->GetNextPageId(), false);
+    buffer_pool_manager_->UnpinPage(cur_node_->GetPageId(), false);
     cur_node_ = next_node;
     index_ = 0;
   }
